2963b8afd5964518facc9a0c147934d60a555ae3
parthergk <gauravkumar81464@gmail.com>
2025-05-17 22:41:16 +0530

    added font faimly and maked header responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,17 +4,17 @@ import React from 'react'
 const Header = () => {
     const dark = false;
   return (
-    <header className=' absolute z-10 w-full flex justify-between items-center px-7 py-6'>
+    <header className='absolute z-10 w-full flex justify-between items-center p-5 md:px-7 md:py-6'>
 
         {/* logo */}
-        <div className=' flex justify-center items-center'>
-            <Image alt="logo img" src="/img/logo.JPG" width={45} height={44} className=' rounded-[5px] '/>
-            <span className=' self-end'>Parther</span>
+        <div className='flex justify-center items-center gap-1.5'>
+            <Image alt="logo img" src="/img/logo.JPG" width={44} height={44} className=' rounded-[5px] shadow-lg shadow-gray-400 h-[36px] w-[36px]'/>
+            <span className=' self-end text-[16px] sm:text-lg font-light leading-none'>Parther</span>
         </div>
 
         {/* mode */}
-        <div className=' self-end '>
-            <button className='cursor-pointer'>{ dark ? "Go Dark" : "Go Light"}</button>
+        <div className='self-end'>
+            <button className='cursor-pointer text-[16px] sm:text-lg font-light leading-none'>{ dark ? "Go Dark" : "Go Light"}</button>
         </div>
     </header>
   )
