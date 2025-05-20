3f8f2e6c3cab43ae89c3f8d660989a19ed2bdb06
parthergk <gauravkumar81464@gmail.com>
2025-05-20 22:42:34 +0530

    dark mode fix and style into css conversion

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="min-h-screen w-auto relative overflow-hidden">
+    <main className="min-h-screen w-auto relative overflow-hidden dark:bg-[#171717] dark:text-[#d1d1d1]">
       {children}
     </main>
   );
