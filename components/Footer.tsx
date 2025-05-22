d0e94086488f038ab6788f3e2a9b5ed40d367ae9
parthergk <gauravkumar81464@gmail.com>
2025-05-22 18:18:41 +0530

    added social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import {
+  GithubIcon,
+  InstagramIcon,
+  Linkedin,
+  TwitchIcon,
+  TwitterIcon,
+} from "lucide-react";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='w-full mt-[65px] md:mt-20 2xl:mt-28 '>
-        <div className=' flex justify-between items-center px-5 py-3 border-t border-neutral-700'>
-            <div>Contact</div>
-            <div className=' text-lg'>Copyright 2025 © Parther</div>
+    <div className="w-full mt-[65px] md:mt-20 2xl:mt-28 ">
+      <div className=" flex flex-col gap-2 sm:flex-row justify-between items-center px-5 py-3 border-t border-neutral-700">
+        <div className=" flex justify-center items-center gap-4">
+          <a href="/">
+            <GithubIcon />
+          </a>
+          <a href="/">
+            <TwitterIcon />
+          </a>
+          <a href="/">
+            <Linkedin />
+          </a>
+          <a href="/">
+            <InstagramIcon />
+          </a>
         </div>
+        <div className=" text-lg">Copyright 2025 © Parther</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
