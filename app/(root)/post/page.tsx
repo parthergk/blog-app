9f18b9f887f8c1c66912ef1c1601102898c9317a
parthergk <gauravkumar81464@gmail.com>
2025-05-29 17:06:43 +0530

    similer post

--- app/(root)/post/page.tsx
+++ app/(root)/post/page.tsx
@@ -6,35 +6,46 @@ import Note from "@/components/post/Note";
 import OutPutCard from "@/components/cards/OutPutCard";
 import PostHeader from "@/components/post/PostHeader";
 import Tip from "@/components/post/Tip";
+import BlogCard from "@/components/cards/BlogCard";
 
 const BlogPost = () => {
+  const array = [1, 2];
   return (
-    <div className="max-w-3xl mx-auto px-4 py-6 font-serif mt-12 md:mt-16 lg:mt-24">
-      <div
-        className=" hidden dark:block h-full min-h-screen overflow-hidden absolute top-0 left-0 right-0 z-0 pointer-events-none flex-none order-[1008]"
-        style={{
-          WebkitMask:
-            "linear-gradient(0deg, rgba(0, 0, 0, 0) 7.5%, rgba(0, 0, 0, 1) 86%)",
-          mask: "linear-gradient(0deg, rgba(0, 0, 0, 0) 7.5%, rgba(0, 0, 0, 1) 86%)",
-        }}
-      >
+    <div className="w-full">
+      <div className="max-w-3xl mx-auto px-4 py-6 font-serif mt-12 md:mt-16 lg:mt-24">
         <div
-          className=" w-full h-full opacity-5"
+          className=" hidden dark:block h-full min-h-screen overflow-hidden absolute top-0 left-0 right-0 z-0 pointer-events-none flex-none order-[1008]"
           style={{
-            backgroundImage:
-              "url('https://framerusercontent.com/images/rR6HYXBrMmX4cRpXfXUOvpvpB0.png')",
-            backgroundSize: "128px",
-            backgroundRepeat: "repeat",
+            WebkitMask:
+              "linear-gradient(0deg, rgba(0, 0, 0, 0) 7.5%, rgba(0, 0, 0, 1) 86%)",
+            mask: "linear-gradient(0deg, rgba(0, 0, 0, 0) 7.5%, rgba(0, 0, 0, 1) 86%)",
           }}
-        ></div>
-      </div>
-      <PostHeader />
-      <DemoCard />
-      <CodeExplanation />
-      <CodeCard />
-      <OutPutCard />
-      <Note />
-      <Tip />
+        >
+          <div
+            className=" w-full h-full opacity-5"
+            style={{
+              backgroundImage:
+                "url('https://framerusercontent.com/images/rR6HYXBrMmX4cRpXfXUOvpvpB0.png')",
+              backgroundSize: "128px",
+              backgroundRepeat: "repeat",
+            }}
+          ></div>
+        </div>
+        <PostHeader />
+        <DemoCard />
+        <CodeExplanation />
+        <CodeCard />
+        <OutPutCard />
+        <Note />
+        <Tip />
+          </div>
+        <div className="py-12 md:py-16 lg:py-24 px-5 md:px-7 ">
+          <div className="flex flex-col md:flex-row items-center justify-center gap-5 sm:gap-10 ">
+          {array.map((item) => (
+            <BlogCard key={item} />
+          ))}
+          </div>
+        </div>
     </div>
   );
 };
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full mt-12 md:mt-16 lg:mt-24 ">
+    <div className="w-full mt-12 md:mt-16 lg:mt-24">
       <div className=" flex flex-col gap-2 sm:flex-row justify-between items-center px-5 py-3 border-t border-neutral-700">
         <div className=" flex justify-center items-center gap-4">
           <a href="/">
