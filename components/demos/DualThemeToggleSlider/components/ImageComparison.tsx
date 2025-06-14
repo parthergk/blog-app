7f38c6e444e3139d8eb25c2374edd2c19bb4c5f0
parthergk <gauravkumar81464@gmail.com>
2025-06-14 11:07:51 +0530

    added draggable divider

--- components/demos/DualThemeToggleSlider/components/ImageComparison.tsx
+++ components/demos/DualThemeToggleSlider/components/ImageComparison.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ImageComparison = () => {
+const ImageComparison = ({ translateX }: { translateX: number }) => {
   return (
     <div className="isolate flex h-full w-full items-center justify-center">
       <div className="h-[30.5rem] w-[375px] relative grid grid-cols-1 grid-rows-1 overflow-hidden rounded-t-4xl bg-gray-950/10 outline outline-gray-950/10 dark:outline-white/10">
@@ -9,6 +9,7 @@ const ImageComparison = () => {
             src="/DualThemeToggleSlider/img/light.png"
             className={`absolute inset-0`}
             alt="Light"
+            style={{clip: `rect(0px, ${translateX - 1}px, 488px, 0px)`,}}
           />
         </div>
         <div className="col-start-1 row-start-1">
--- components/demos/DualThemeToggleSlider/index.tsx
+++ components/demos/DualThemeToggleSlider/index.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageComparison from "./components/ImageComparison";
 
 const index = () => {
+  const [translateX, setTranslateX] = useState(187.5);
   return (
     <div className="bg-[#030712] p-5  flex justify-center items-center m-auto">
       <div className="h-112 p-4 sm:p-8 relative overflow-hidden rounded-lg border border-gray-800 bg-gray-950/[2.5%] bg-[image:radial-gradient(var(--pattern-fg)_1px,_transparent_0)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-white)]/10">
         <div className=" w-full overflow-hidden">
-          <ImageComparison/>
+
+          {/* Draggable Divider */}
+          <div
+            className="absolute inset-y-0 z-10 w-1 bg-sky-400 cursor-ew-resize"
+            style={{ transform: `translateX(${translateX}px)` }}
+          ></div>
+          
+          <ImageComparison translateX={translateX} />
         </div>
       </div>
     </div>
